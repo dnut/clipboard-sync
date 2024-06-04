655c6641455aa48f2e1824dfbd5d1a71af91612b
QuarterHeaven <liaotx2@gmail.com>
2024-06-04 11:14:21 +0800

    add service config in nix flake

--- flake.nix
+++ flake.nix
@@ -8,9 +8,24 @@
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./. { };
-      });
-    };
+      in {
+	packages = forAllSystems (system: {
+          default = pkgsFor.${system}.callPackage ./. { };
+	});
+
+	nixosModules.default =
+	  # For illustration, probably want to break this definition out to a separate file
+	  { config, pkgs, lib, ... }: {
+            options = {
+              services.myApp.enable = lib.mkEnableOption "clipboard-sync";
+            };
+
+            config = lib.mkIf config.services.clipboard-sync.enable {
+              systemd.services.clipboard-sync = {
+		# Insert systemd config here
+		serviceConfig.ExecStart = "${self.packages.${pkgs.system}.default}/bin/main";
+              };
+            };
+	  };
+      };
 }
